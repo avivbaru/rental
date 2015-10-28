ba2ca82014fcecd05c32480774d03a23e1930462
avivbaru <aviv@taykey.com>
2015-10-29 00:23:52 +0200

    nicier

--- rental.py
+++ rental.py
@@ -2,33 +2,31 @@
 
 class User(object):
     def __init__(self, user_name, first_name, last_name, **kwargs):
-    	self.user_name = user_name
-    	self.first_name = first_name
-    	self.last_name = last_name
+        self.user_name = user_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.rate = 0
         self.comments = []
         self.offer = []
         self.rental = []
 
-
     def add_offer(self, offer):
-    	self.offer.append(offer)
+        self.offer.append(offer)
 
     def add_rental(self, rental):
-    	self.rental.append(rental)
+        self.rental.append(rental)
 
     def add_comment(self, comment):
-    	self.comments.append(comment)
+        self.comments.append(comment)
 
     def calculate_rate(self):
-    	rates = [int(i.get("rate")) for i in self.comments]
+        rates = [int(i.get("rate")) for i in self.comments]
         self.rate = reduce(lambda x, y: x + y, rates) / float(len(rates))
 
 
-
 class Product(object):
-	def __init__(self, name, category, **kwargs):
-		self.id = generate_id()
+    def __init__(self, name, category, **kwargs):
+        self.id = self.generate_id()
         self.name = name
         self.desciption = None
         self.price = None
@@ -37,5 +35,6 @@ class Product(object):
         self.start_date = None
         self.end_date = None
 
-    def generate_id():
+    def generate_id(self):
         return ""
+
